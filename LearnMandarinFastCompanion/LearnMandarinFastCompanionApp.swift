945d6443e8ccef90f897709d998fcc08349cf36e
James Dooley <jimbdooley@gmail.com>
2024-10-02 22:49:44 -0600

    Initial Commit

---
+++ LearnMandarinFastCompanion/LearnMandarinFastCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnMandarinFastCompanionApp.swift
+//  LearnMandarinFastCompanion
+//
+//  Created by James Dooley on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnMandarinFastCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
